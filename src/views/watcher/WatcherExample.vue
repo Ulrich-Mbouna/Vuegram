ba0b7cbc9c88b8c1eb1f8c592cb0502a06048f25
MbounaUlrich <mbouna@kiroogames.com>
2021-03-25 12:37:11 +0100

    Update

---
+++ src/views/watcher/WatcherExample.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>Watchers in Vue 3 </h2>
+        <input type="text" v-model="val">
+    </div>
+</template>
+<script>
+export default {
+    const val = ref(null);
+}
+</script>
+<style lang="">
+    
+</style>
